1740f89bb65c0dd0407a30bdcccfc45b068d4a9b
colonelxy <harold2@protonmail.com>
2022-10-16 21:00:26 +0300

    done with hardhat basics

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,8 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-numbers")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 // task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 //     const accounts = await hre.ethers.getSigners()
@@ -14,12 +16,21 @@ require("./tasks/block-numbers")
 // })
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP || "key"
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "KES",
+        coinmarketcap: COIN_MARKET_CAP_API_KEY,
+        token: "MATIC",
     },
     defaultNetwork: "hardhat",
     networks: {
@@ -27,6 +38,11 @@ module.exports = {
             url: GOERLI_RPC_URL,
             accounts: [METAMASK_PRIVATE_KEY],
             chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+            // accounts:
         },
     },
     solidity: "0.8.17",
